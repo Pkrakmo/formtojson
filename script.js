52a95bf33d4246566fda4f3165dd0e7d20abcef7
Paul <paul.kraakmo@gmail.com>
2020-03-09 14:41:58 +0100

    Restructure

---
+++ script.js
@@ -0,0 +1,26 @@
+const subBtn = document.getElementById('subBtn')
+const rstBtn = document.getElementById('rstBtn')
+const cpyBtn = document.getElementById('cpyBtn')
+const completeBtn = document.getElementById('completeBtn')
+const testBtn2 = document.getElementById('testBtn2')
+
+
+subBtn.addEventListener('click', function () {
+    formToJson()
+})
+
+rstBtn.addEventListener('click', function () {
+    resetForm()
+})
+
+cpyBtn.addEventListener('click', function () {
+    copyToClipboard()
+})
+
+completeBtn.addEventListener('click', function () {
+    completeJson()
+})
+
+testBtn2.addEventListener('click', function () {
+    console.log("😅")
+})
